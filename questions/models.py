080c1274da14ed65b972e306421e9e924ccca978
Sawa Demidenko <sdemidenko@ivi.ru>
2017-10-30 18:51:28 +0300

    Добавление кастомного юзера.

--- questions/models.py
+++ questions/models.py
@@ -2,10 +2,18 @@
 from __future__ import unicode_literals
 
 from datetime import datetime
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class User(AbstractUser):
+    upload = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+
+
 class Question(models.Model):
+    author = models.ForeignKey(User)
+
     title = models.CharField(max_length=120, verbose_name=u"Заголовок вопроса")
     text = models.TextField(verbose_name=u"Полное описание вопроса")
 
