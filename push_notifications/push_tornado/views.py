f0d363a4180f6bd3954618d338571e6b47b20f79
Sawa Demidenko <sdemidenko@ivi.ru>
2017-12-12 14:16:09 +0300

    Добавим реализацию в Django для работы через tornado

---
+++ push_notifications/push_tornado/views.py
@@ -0,0 +1,33 @@
+import json
+from tornado.web import RequestHandler
+from tornado.websocket import WebSocketHandler
+REGISTRY = {}
+
+
+class MainHandler(RequestHandler):
+    def post(self):
+        body = self.get_argument("msg")
+        uid = self.get_argument("uid")
+        conn = REGISTRY.get(uid)
+        if conn:
+            conn.write_message(json.dumps({"msg": body}))
+            self.write("OK")
+        else:
+            self.write("NO")
+
+
+class WSHandler(WebSocketHandler):
+    def open(self, channel):
+        print("WebSocket opened")
+        self.uid = channel
+        REGISTRY[self.uid] = self
+
+    def on_message(self, message):
+        self.write_message(json.dumps({"msg": "hi, dog"}))
+
+    def check_origin(self, origin):
+        return True
+
+    def on_close(self):
+        print("WebSocket closed")
+        del REGISTRY[self.uid]
