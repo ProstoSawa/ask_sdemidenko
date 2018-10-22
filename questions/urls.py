f3154b0e22946a08135e2de3ce1e7dce92cbb496
Sawa Demidenko <sdemidenko@ivi.ru>
2018-10-23 00:13:57 +0300

    Правки необходимые для работы с Django2

--- questions/urls.py
+++ questions/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
 from questions.views import questions_detail, QuestionList
 
 urlpatterns = [
-    url(r'^$', QuestionList.as_view(), name='questions_list'),
-    url(r'^(?P<question_id>[0-9]+)/$', questions_detail, name='questions_detail'),
+    path('', QuestionList.as_view(), name='questions_list'),
+    path('<int:number>/', questions_detail, name='questions_detail'),
 ]
